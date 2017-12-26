52a03499e403ebaa92fc66e30e60dcaf46725c35
Gabriel Duarte <gabrielqduarte@gmail.com>
2017-12-26 18:04:40 -0200

    Use the normalAdWaitSeconds option as the amount of seconds to wait before closing normal ads

--- src/options.ts
+++ src/options.ts
@@ -3,8 +3,8 @@ const saveOptions = () => {
   const videoAdWaitSeconds: string = (document.getElementById("videoAdWaitSeconds") as HTMLInputElement).value
   chrome.storage.sync.set(
     {
-      normalAdWaitSeconds,
-      videoAdWaitSeconds,
+      normalAdWaitSeconds: Number(normalAdWaitSeconds) * 1000,
+      videoAdWaitSeconds: Number(videoAdWaitSeconds) * 1000,
     },
     () => {
       const status = document.getElementById("status") as HTMLElement
@@ -19,13 +19,13 @@ const saveOptions = () => {
 const restoreOptions = () => {
   chrome.storage.sync.get(
     {
-      normalAdWaitSeconds: 5,
-      videoAdWaitSeconds: 5,
+      normalAdWaitSeconds: 5000,
+      videoAdWaitSeconds: 5000,
     },
     ({ normalAdWaitSeconds, videoAdWaitSeconds }) => {
       // tslint:disable-next-line:semicolon
-      ;(document.getElementById("normalAdWaitSeconds") as HTMLInputElement).value = normalAdWaitSeconds
-      ;(document.getElementById("videoAdWaitSeconds") as HTMLInputElement).value = videoAdWaitSeconds
+      ;(document.getElementById("normalAdWaitSeconds") as HTMLInputElement).value = `${normalAdWaitSeconds / 1000}`
+      ;(document.getElementById("videoAdWaitSeconds") as HTMLInputElement).value = `${videoAdWaitSeconds / 1000}`
     },
   )
 }
--- src/youtube-autoclose-ads.ts
+++ src/youtube-autoclose-ads.ts
@@ -1,15 +1,23 @@
 let adFound: boolean = false
 setInterval(() => {
-  const adClose: HTMLElement | null = document.querySelector(".adDisplay .close-button")
-  const videoAdClose: HTMLElement | null = document.querySelector(".videoAdUiSkipButton")
-  if (adClose && !adFound) {
-    setTimeout(() => {
-      adClose.click()
-      adFound = false
-    }, 5000)
-    adFound = true
-  }
-  if (videoAdClose) {
-    videoAdClose.click()
-  }
+  chrome.storage.sync.get(
+    {
+      normalAdWaitSeconds: 5,
+      videoAdWaitSeconds: 5,
+    },
+    ({ normalAdWaitSeconds, videoAdWaitSeconds }) => {
+      const adClose: HTMLElement | null = document.querySelector(".adDisplay .close-button")
+      const videoAdClose: HTMLElement | null = document.querySelector(".videoAdUiSkipButton")
+      if (adClose && !adFound) {
+        setTimeout(() => {
+          adClose.click()
+          adFound = false
+        }, normalAdWaitSeconds)
+        adFound = true
+      }
+      if (videoAdClose) {
+        videoAdClose.click()
+      }
+    },
+  )
 }, 100)
